69f89a0d26f2ba67d25712d0a182954bd9c07d98
marissa-mayes <marissa.mayes1289@gmail.com>
2020-08-13 21:45:35 -0500

    handle bars and server connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+var exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,9 @@ const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));

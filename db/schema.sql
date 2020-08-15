ac81d37ee7a8c83c4f7096d3305f9d82a8b27776
Arpita K Sau <arpitakaran@gmail.com>
2020-08-15 10:33:55 -0500

    sign uu and login working

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
-CREATE database travelINstyle_db;
-USE travelINstyle_db;
+CREATE database travelNstyle_db;
+USE travelNstyle_db;
 
 CREATE TABLE destination 
 (
-    id init NOT NULL AUTO INCREMENT,
+    id integer NOT NULL AUTO_INCREMENT,
     destination varchar (200) NOT NULL,
     address varchar(200) NOT NULL,
-    PRIMARY KEY (ID),
-
-
+    PRIMARY KEY (ID)
 )
 
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -30,7 +29,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",

8471f56695fe43d5960014adb3982bc7d8683ab1
marissa-mayes <marissa.mayes1289@gmail.com>
2020-08-15 10:13:16 -0500

    db files added

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE database travelINstyle_db;
+USE travelINstyle_db;
+
+CREATE TABLE destination 
+(
+    id init NOT NULL AUTO INCREMENT,
+    destination varchar (200) NOT NULL,
+    address varchar(200) NOT NULL,
+    PRIMARY KEY (ID),
+
+
+)
+

8ab064ad2df48332a304a94b4976620d4a601101
Brianna Bullock <brianna.bullock16@gmail.com>
2020-08-20 21:00:00 -0500

    simplified clothing logic

--- models/outfit.js
+++ models/outfit.js
@@ -1,8 +1,10 @@
 //Creation of trip model//
 module.exports = function(sequelize, DataTypes) {
   const Outfit = sequelize.define("Outfit", {
+    name: { type: DataTypes.STRING },
     top: { type: DataTypes.STRING },
     bottom: { type: DataTypes.STRING },
+    shoe: { type: DataTypes.STRING },
     accessory: { type: DataTypes.STRING }
   });
 
